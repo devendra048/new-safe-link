9cad58f0a8311666ae8c48f53a2a51dc8e158d53
devendra048 <53045178+devendra048@users.noreply.github.com>
2019-07-19 14:32:33 +0530

    Update golink.js

--- golink.js
+++ golink.js
@@ -131,7 +131,7 @@ auto_safeconvert () function {
         }
         if (a_to_vh == false) {
             var encryptedUrl = Base64.encode (a_to_vi);
-            a_to_ve [i]. href = "http://www.4gtrick.com/golink/?url=" + encryptedUrl;
+            a_to_ve [i]. href = "https://www.4gtrick.com/?page_id=21?url=" + encryptedUrl;
             a_to_ve [i] .rel = "nofollow";
             a_to_vb ++;
             a_to_vc + = i + ":::" + a_to_ve [i] .href + "\ n";
